6745b04100b8e2b9a97124a9de424fb7d6d1af78
Haye Bohm <haye.bohm@gmail.com>
2015-02-05 22:57:04 +0100

    Added frequencies of each letter in file. Improved readability and comments.

--- Assignment1/exercise2/verificating/parsegenome.c
+++ Assignment1/exercise2/verificating/parsegenome.c
@@ -2,34 +2,68 @@
 #include <string.h>
 #include <stdbool.h>
 
-int check_valid_genomes(FILE *file) {
+bool check_count_character(char ch, int *pa, int *pc, int *pt, int *pg) ;
+bool valid_genomes_file(FILE *file, int *pa, int *pc, int *pt, int *pg);
+
+bool check_count_character(char ch, int *pa, int *pc, int *pt, int *pg) {
+  if (ch == 'A') {
+    *pa += 1 ;
+    return true ;
+  }
+  else if (ch == 'C') {
+    *pc += 1 ;
+    return true ;
+  }
+  else if (ch == 'T') {
+    *pg += 1 ;
+    return true ;
+  }
+  else if (ch == 'G') {
+    *pt += 1 ;
+    return true ;
+  }
+  else {
+    return false ;
+  }   
+}
+
+// Checks all characters in *file and returns -1 if file is not a correct genome file, 1 if file is a correct genome file.
+bool valid_genomes_file(FILE *file, int *pa, int *pc, int *pt, int *pg) {
     char ch, old_ch;
     int count = 0 ;
     int linecount = 1;
-    int correct = 0;
+    bool correct = true;
+
+    // Loop through file
     while ((ch = fgetc(file)) != EOF) {
-        old_ch = ch ; 
+        old_ch = ch ;
+	// count number of characters in line
         if (ch != '\n') {
-            if (ch == 'A' || ch == 'T' || ch == 'C' || ch == 'G') {
-                count += 1 ;
-            }
+	  if (check_count_character(ch, pa, pc, pt, pg)){
+	      count += 1 ;
+	    }
         }
         else {
+	  // print if number of characters in a line is not correct
             if (count != 100) {
                 fprintf(stderr, "File contains error on line: %i\n", linecount);
-                correct = -1 ;           
+                correct = false ;           
             }
             count = 0 ;
             linecount += 1 ;
        } 
-    } 
+    }
+
+    // If the second to last character is a newline, we've counted 1 line too much
     if (old_ch == '\n') {
        linecount -= 1 ;
-    }    
+    }
+
+    // Print if the number of lines is not correct
     if (linecount != 500) {
         fprintf(stderr, "File does not have the correct amount of lines.\n");
         fprintf(stderr, "Number of lines: %d\n", linecount) ;
-        return -1 ;    
+        return false ;    
     }
     return correct;
 }
@@ -44,17 +78,32 @@ int main(int argc, char *argv[]) {
     FILE *fp;
     fp = fopen(argv[1], "r");
 
-    if (fp  == NULL) {
-       fprintf(stderr, "Can't open the file!\n") ;
-       return -1  ;
-    } 
+    int freq_a = 0;
+    int freq_c = 0;
+    int freq_g = 0 ;
+    int freq_t = 0;
+    int *pa = &freq_a ;
+    int *pc = &freq_c;
+    int *pg = &freq_g;
+    int *pt = &freq_t;
     
-    if (check_valid_genomes(fp) == 0) {
-        fprintf(stdout, "Genome file validated.\n");
+    if (fp  == NULL) {
+      fprintf(stderr, "Can't open file \"%s\".\n", argv[1]) ;
+       return -1  ;
+    }
+    
+    if (valid_genomes_file(fp, pa, pc, pt, pg)) {
+      fprintf(stdout, "Genome file \"%s\" validated.\n", argv[1]);
+	fprintf(stdout, "Frequencies: \n");
+	fprintf(stdout, "A: %d\n", freq_a) ;
+	fprintf(stdout, "C: %d\n", freq_c) ;
+	fprintf(stdout, "T: %d\n", freq_t) ;
+	fprintf(stdout, "G: %d\n", freq_g) ;
+	
         return 0;
     }
     else {
-        fprintf(stderr, "Genome file is corrupt or incorrect.\n");
+      fprintf(stderr, "Genome file \"%s\" is corrupt or incorrect.\n", argv[1]);
         return -1;
     }
 } 
