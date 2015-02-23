34a53ea2b47409f642cb61c9f8b94f35e0f43127
Haye Böhm <haye.bohm@gmail.com>
2015-02-23 17:30:01 +0100

    Started exercise 3.1

---
+++ Assignment3/exercise1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* The program will figure out the memory usage on the stack of magic_function() */ 
+int main (void) {
+   
+  int time = 0 ;
+  
+
+  return 0 ;
+}
---
+++ Assignment3/magic_function.c
@@ -0,0 +1,8 @@
+
+void magic_function() {
+  int number = 1234 ;
+  int number2 = 12345 ;
+  int multiplier = 12 ;
+  int result = (number + number2) * multiplier ;
+  Return 0 ;
+}
