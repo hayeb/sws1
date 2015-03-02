7b8ed086e029868dd671af67ccdb1f47dad7bc2f
mmwvh <wvhoorn92@gmail.com>
2015-03-02 09:46:52 +0100

    Changes to exercise1.c, added a heap_array

--- Assignment3/exercise1/exercise1.c
+++ Assignment3/exercise1/exercise1.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "magic_function.h"
 
-char* create_4mb_array() {
+void create_4mb_array() {
     const int size = 4194304;
     char array[size] ;
     int i = 0;
     for (i = 0; i < size; i++) {
 	array[i] = 'X';
     }
-    char *p = array;
-    return p;
 }
 
 int main(void) {
     const int size = 4194304;
-    create_4mb_array() ;
-    unsigned char* p =(unsigned char*) create_4mb_array() ;
     int i;
+    const int* p = &i;
     unsigned long bytes = 0 ;
     int works = 0 ;
+    char* heap_array = malloc(size);
+
+    create_4mb_array() ;
     magic_function() ;
+
     for (i = 0; i < size; i++) {
-	printf("%p is %c\n", p+i,*(p+i));
-	if (*(p+i) != 'X') { 	
-	    bytes ++;
-	    works = 1 ;
-	}
+	*(heap_array+i) = *(p+i);	
     }
     printf("magic_function uses %lu bytes. Works: %d\n", bytes, works) ;
 }
